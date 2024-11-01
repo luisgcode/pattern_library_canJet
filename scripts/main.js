5988bdc104503f6fb7c3eb7e21845ab1e58e3fe2
luisgcode <luisgcode@gmail.com>
2024-11-01 16:44:31 -0400

    Adding mobile menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,7 @@
 "use strict";
+const mobileMenu = document.getElementById("mobile-menu");
+const mobileNav = document.querySelector(".mobile-nav");
+
+mobileMenu.addEventListener("click", () => {
+  mobileNav.classList.toggle("active");
+});
